e970a7ce95188429b64acc03a90a4a959621ddfc
Baskar Rao <baskarrao.dandlamudi@outlook.com>
2021-10-21 11:32:39 -0400

    Update version
    Update

--- testapp/vue.config.js
+++ testapp/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         "/api": {
-          target: "https://auth0-sample-code.vercel.app/"
+          target: "https://auth0-sample-code.vercel.app"
         }
       }
     }
